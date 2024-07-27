ebe78160af3015cd7a76b1a289c75d0ba7664d7e
Hariharan A <hariharananbalagan1268@gmail.com>
2024-07-27 11:32:14 +0530

    modified:   app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-JG2ZJVQT" />
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
@@ -27,7 +28,6 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      <GoogleTagManager gtmId="GTM-JG2ZJVQT" />
     </html>
   );
 }
