f2df624ec2018cdd26f43cc2f8e7b0e34f2f28b1
Hariharan A <120185640+Hariharan-A-1268@users.noreply.github.com>
2025-07-15 15:08:00 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,19 +1,25 @@
 export const experiences = [
   {
     id: 1,
-    title: "Software Engineering Fellowship",
-    company: "Headstarter AI",
-    duration: "(Jul 2024 - Present)",
+    title: "IT Trainee",
+    company: "FIS",
+    duration: "(Jan 2025 - Present)",
   },
   {
     id: 2,
+    title: "Software Engineer Fellowship",
+    company: "Headstarter AI",
+    duration: "(Jul 2024 - Aug 2024)",
+  },
+  {
+    id: 3,
     title: "Internship Trainee",
     company: "NLC India Limited",
     duration: "(Sep 2023 - Oct 2023)",
   },
   {
-    id: 3,
-    title: "Intern",
+    id: 4,
+    title: "Web Developer Intern",
     company: "ITI Limited",
     duration: "(Aug 2023 - Sep 2023)",
   },
